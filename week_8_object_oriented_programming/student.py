6c8b3babbd622233e877ee9411e44a15d6d13499
Viktor <v.rudenko@triacom.ua>
2023-10-11 15:36:54 +0300

    Complete the half of the OOP lesson

--- week_8_object_oriented_programming/student.py
+++ week_8_object_oriented_programming/student.py
@@ -1,13 +1,44 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def house(self):
+        return self._house
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    student._house = "Nuber Four, Privet Drive"
+    # student.house = "Number Four, Privet Drive"
+    print(student)
 
 
 def get_student():
-    student = {"name": input("Name: "), "house": input("House: ")}
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+
+    return Student(name, house)
 
 
 if __name__ == '__main__':
